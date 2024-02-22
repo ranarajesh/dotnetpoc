26e0f6842c367254d516c38e85a8cc2677fa0105
Rana Rajesh <rajesh86.rana@gmail.com>
2024-02-23 00:23:22 +0530

    Containduplicate file

---
+++ console-poc/Array/ContainsDuplicate.cs
@@ -0,0 +1,16 @@
+﻿namespace console_poc;
+public class ContainsDuplicate
+{
+    public static bool containDuplicate(int[] arr){
+        bool isDuplicate = false;
+        var seen = new HashSet<int>();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            var item = arr[i];
+            if(!seen.Add(item)){
+                return true;
+            }
+        }
+        return isDuplicate;
+    }
+}
